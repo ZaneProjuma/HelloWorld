aaa1b94cf3044bf4a15896f8f2a7b2b61613b7e3
ZaneProjuma <zaneprojuma@gmail.com>
2021-08-30 20:55:12 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("dadada");
+        System.out.println("Hello World!!!");
+        System.out.println("Second Hello World");
+    }
+}
